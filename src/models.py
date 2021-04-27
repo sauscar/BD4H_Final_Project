98852c8bf55cd35095455a319a0d51d0904ed713
oscar <oparrilla96@gmail.com>
2021-04-27 11:17:37 -0400

    update2

--- src/models.py
+++ src/models.py
@@ -2,6 +2,12 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split, cross_val_predict, KFold
 from sklearn import metrics
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import KFold, cross_val_predict, train_test_split
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
 
 def logreg(X,y):
    
@@ -22,16 +28,55 @@ def logreg(X,y):
         roc = metrics.roc_auc_score(pred_values , y_test)
         roc_score.append(round(roc,4))
 
-        acc = metrics.accuracy_score(y_test,pred_values)
-        acc_score.append(round(acc,4))
+        roc = metrics.roc_auc_score(y_test,pred_values)
+        roc_score.append(roc)
 
-    avg_roc_score = sum(roc_score)/k
-    avg_acc_score = sum(acc_score)/k
-    print('ROC of each fold - {}'.format(roc_score))
-    print('Avg ROC : {}'.format(round(avg_roc_score,4)))
+    avg_roc_score = sum(roc_score) / k
 
-    print('Accuracy of each fold - {}'.format(acc_score))
-    print('Avg Accuracy : {}'.format(round(avg_acc_score,4)))
-    
+    print("ROC of each fold - {}".format(roc_score))
+    print("Avg ROC : {}".format(avg_roc_score))
 
+    class_names = ['NEGATIVE', 'SEPSIS']
+
+    resultMatrix = confusion_matrix(y_test,pred_values, normalize = 'true')
+    disp = ConfusionMatrixDisplay(confusion_matrix = resultMatrix, display_labels = class_names)
+    disp = disp.plot(include_values = True, cmap = 'Blues', xticks_rotation=45)
+    plt.show()
+
+
+def lightgbm(X, y):
+
+    k = 5
+    kf = KFold(n_splits=k, random_state=None)
+    lgbm = LGBMClassifier()
+
+    roc_score = []
+    acc_score = []
+
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X.todense()[train_index, :], X.todense()[test_index, :]
+        # [:10, :10]
+        y_train, y_test = np.array(y)[train_index], np.array(y)[test_index]
+        lgbm.fit(X_train, y_train)
+        pred_values = lgbm.predict(X_test)
+
+        roc = metrics.roc_auc_score(y_test, pred_values)
+        avg = metrics.accuracy_score(y_test,pred_values)
+        roc_score.append(roc)
+        acc_score.append(avg)
+
+
+    avg_roc_score = sum(roc_score) / k
+    avg_accuracy_score = sum(acc_score)/k
+    print("ROC of each fold - {}".format(roc_score))
+    print("Avg ROC : {}".format(avg_roc_score))
+    print("Accuracy of each fold - {}".format(acc_score))
+    print("Avg Accuracy: {}".format(avg_accuracy_score))
+
+    class_names = ['NEGATIVE', 'SEPSIS']
+
+    resultMatrix = confusion_matrix(y_test,pred_values, normalize = 'true')
+    disp = ConfusionMatrixDisplay(confusion_matrix = resultMatrix, display_labels = class_names)
+    disp = disp.plot(include_values = True, cmap = 'Blues', xticks_rotation=45)
+    plt.show()
 
